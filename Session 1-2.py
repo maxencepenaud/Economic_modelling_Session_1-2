df241364e96165f029e9264bdb1be8c398266140
maxencepenaud <mpenaud.ieu2022@student.ie.edu>
2024-04-05 08:55:31 +0200

    hello

--- Session 1-2.py
+++ Session 1-2.py
@@ -1,8 +1,6 @@
 import random
-
-
 class Point:
-    def init(self, x, y):
+    def _init_(self, x, y):
         """
         This will be called when instantiating an object
         :param x: the value of x
@@ -10,29 +8,27 @@ class Point:
         """
         self.x = x
         self.y = y
-
-    def str(self):
+    def _str_(self):
         """
         This will return the string value used in printing the point
         :return:
         """
         return f"({self.x}, {self.y})"
 
-    def repr(self):
+    def _repr_(self):
         """
         This is when the point is in a list or other container
         :return:
         """
 
-        return self.str()
-
-
-a = Point(2, 3)
-b = Point(7, 9)
+        return self._str_()
+a = Point(2,3)
+b = Point(7,9)
 print(f"a=({a.x}, {a.y})")
 print(f"b=({b.x}, {b.y})")
 
-points = []
+
+points  = []
 for _ in range(5):
     x = random.randint(-100, 100)
     y = random.randint(-100, 100)
@@ -46,8 +42,8 @@ points.append(Point(random.randint(-100, 100),
 for point in points:
     print(f"p({point.x}, {point.y})")
 
-# try to print the first point
+#try to print the first point
 print("printing a point value", points[0])
 print(points)
-a = point(3, 4)
+a = point(3,4)
 print(f"distance origin a= {a.distance_origin}")
